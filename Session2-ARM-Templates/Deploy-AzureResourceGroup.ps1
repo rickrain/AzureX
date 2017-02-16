f01073ec75fd6276130f0fec17d1cdc40d864cf6
Rick Rainey <rickra@microsoft.com>
2017-02-16 16:05:06 -0600

    Add architecture diagram for session 3.  Updated default resource group name for session 2.

--- Session2-ARM-Templates/Deploy-AzureResourceGroup.ps1
+++ Session2-ARM-Templates/Deploy-AzureResourceGroup.ps1
@@ -4,7 +4,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'AzureX',
+    [string] $ResourceGroupName = 'AzureX-Session-2',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
